b1242284abeefe10b64fd7a005b279ed11a84578
Jules <562448@student.fontys.nl>
2024-11-27 15:50:38 +0100

    Bugfix

--- Infrastructre/UserRepository.cs
+++ Infrastructre/UserRepository.cs
@@ -10,10 +10,10 @@ public class UserRepository
         _context = context;
     }
 
-    public List<User> GetUsers()
-    {
-        // Use raw SQL to query the 'accounts' table
-        string sqlQuery = "SELECT Id, Username, Email FROM accounts";
-        return _context.Users.FromSqlRaw(sqlQuery).ToList(); // Assumes you still want a list of users, but no automatic mapping
-    }
+    //public List<User> GetUsers()
+    //{
+    //    // Use raw SQL to query the 'accounts' table
+    //    string sqlQuery = "SELECT Id, Username, Email FROM accounts";
+    //    return _context.Users.FromSqlRaw(sqlQuery).ToList(); // Assumes you still want a list of users, but no automatic mapping
+    //}
 }
--- LoginRazorPagesTraining/Program.cs
+++ LoginRazorPagesTraining/Program.cs
@@ -1,4 +1,3 @@
-using Infrastructre;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
