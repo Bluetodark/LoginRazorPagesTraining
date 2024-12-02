c421588ba5850a2610577348d3e00eed95f80d5d
Jules <562448@student.fontys.nl>
2024-12-02 16:45:52 +0100

    changed back the connection string

--- Infrastructre/MyDbContext.cs
+++ Infrastructre/MyDbContext.cs
@@ -6,7 +6,7 @@ namespace Infrastructure
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySql("server=localhost;database=logintraining;user=app;password=app;", new MySqlServerVersion(new Version(8, 2, 12))); // Use your actual MySQL version
+            optionsBuilder.UseMySql("server=localhost;database=logintraining;user=root;password=;", new MySqlServerVersion(new Version(8, 2, 12))); // Use your actual MySQL version
         }
     }
 }
